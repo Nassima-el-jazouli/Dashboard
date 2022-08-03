1c4dd2758943a5e5726541b8257aae4ef06d61c7
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-08-03 15:01:49 +0100

    Responsive Dashboard with Light and Darktheme

--- main.js
+++ main.js
@@ -18,3 +18,33 @@ $(document).ready(function() {
             }
     })
 })
+
+$(document).ready(function () {
+        $('aside .sidebar a').click(function(e) {
+    
+            $('aside .sidebar a.active').removeClass('active');
+    
+            var $parent = $(this).parent();
+            $parent.addClass('active');
+            e.preventDefault();
+        });
+    });
+
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+
+menuBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'none';
+})
+
+themeToggler.addEventListener('click', () => {
+        document.body.classList.toggle('dark-theme-variables');
+
+        themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+        themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
